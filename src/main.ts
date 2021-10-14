c40351be691b6ea90d0a114635880f84e6ce81d5
Alejandro Zielinsky <azielinsky95@gmail.com>
2021-10-14 16:59:48 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const commits = core.getInput('commits')
+    console.log(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
+    console.log(new Date().toTimeString())
     await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    console.log(new Date().toTimeString())
+
+    console.log(`List of commits: ${commits}`)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
