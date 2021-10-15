a225c966550d136298142790f6ab112fb8ae4a8d
AlejandroZ <azielinsky95@gmail.com>
2021-10-15 13:37:48 -0700

    - Init commit

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as api from './coda'
+import * as rowBuilder from './row-builder'
+import { Commit } from './commits'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
+    const commits = JSON.parse(core.getInput('commits')) as Commit[]
+    const token = core.getInput('coda-token')
+    const docId = core.getInput('doc-id')
+    var columns = await api.getColumnsForTable(docId, 'Release 2.0')
+    await api.insertRows(docId, 'Release 2.0', rowBuilder.buildRow(columns, commits[0]))
+ 
+    console.log(" WE ARE RUNNING LATEST ")
+    console.log(`List of column ids: ${columns}`)
+    console.log(`List of commits: ${commits}`)
+    console.log(`token: ${token}`)
+    console.log(`docId: ${docId}`)
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
