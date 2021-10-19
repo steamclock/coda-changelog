44b6dccc46dd89adcc9ce94c4004a2e1261fbd80
Alejandro Zielinsky <azielinsky95@gmail.com>
2021-10-19 09:48:49 -0700

    - Row builder now takes in an array of commits - Added table name / commits since last tag as inputs - Logging all commits since last tag

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,24 @@ import { Commit } from './model/commit'
 
 async function run(): Promise<void> {
   try {
-    const commits = JSON.parse(core.getInput('commits')) as Commit[]
+    
+    var commitsSinceLastTag = JSON.parse(core.getInput('all-commits')) as Commit[]
+    const commit = JSON.parse(core.getInput('commit')) as Commit
+
+    const tableName = core.getInput('table')
     const docId = core.getInput('doc-id')
-    var columns = await api.getColumnsForTable(docId, 'Release 2.0')
-    await api.insertRows(docId, 'Release 2.0', rowBuilder.buildRow(columns, commits[0]))
+
+    var columns = await api.getColumnsForTable(docId, tableName)
+    
+    if (commitsSinceLastTag === undefined || commitsSinceLastTag.length == 0) {
+        await api.insertRows(docId, tableName, rowBuilder.buildRow(columns, [commit]))
+    } else {
+        commitsSinceLastTag.push(commit)
+        await api.insertRows(docId, tableName, rowBuilder.buildRow(columns, commitsSinceLastTag))
+    }
  
-    console.log(`List of column ids: ${columns}`)
-    commits.forEach((commit) => {
-        console.log(`Posting commit : ${commit}`)
-    });
+    console.log(`Commits since last tag: ${commitsSinceLastTag}`)
+    console.log(`Posting commit : ${commit}`)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
--- src/row-builder.ts
+++ src/row-builder.ts
@@ -2,23 +2,27 @@ import { Commit } from './model/commit'
 import { Row, Column, TableCell } from './model/table'
 const moment = require('moment')
 
-export function buildRow(columns: Column[], commit: Commit) {
-    var cells: TableCell[] = []
-    for(var key in columns) {
-        var column = columns[key]
-        var cell = {
-            column: column.id,
-            value: valueForColumn(column.name, commit)
-        } as TableCell
-        cells.push(cell)
-    }
-
-    var row = {
-        cells: cells
-    } as Row
-
+export function buildRow(columns: Column[], commits: Commit[]) {
+    
+    var rows: Row[] = []
+    commits.forEach((commit) => {
+        var cells: TableCell[] = []
+        for(var key in columns) {
+            var column = columns[key]
+            var cell = {
+                column: column.id,
+                value: valueForColumn(column.name, commit)
+            } as TableCell
+            cells.push(cell)
+        }
+        var row = {
+            cells: cells
+        } as Row
+        rows.push(row)
+    })
+    
     return {
-        rows: [row]
+        rows: rows
     }
 }
 
